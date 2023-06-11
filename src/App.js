a62c9a71959feda464799a48006e4dc7ab4e3a68
guiziis <guigamoda@hotmail.com>
2023-06-11 15:08:42 -0300

    feat: testando render com map do js

--- src/App.js
+++ src/App.js
@@ -1,45 +1,41 @@
-import logo from './logo.svg';
 import { Component } from 'react';
 import './App.css';
 
 class App extends Component {
- state = {
-      name: 'teste',
-      counter: 0
-    }
-
-  handlePClick = () => {
-    this.setState({name: 'teste2'});
-  }
-
-  handleAclick = (event) => {
-    event.preventDefault();
-    const { counter } = this.state;
-    this.setState({counter: counter + 1});
+  state = {
+    posts: [
+      {
+        id: 1,
+        title: 'teste 1',
+        body: 'body 1'
+      },
+      {
+        id: 2,
+        title: 'teste 2',
+        body: 'body 2'
+      },
+      {
+        id: 3,
+        title: 'teste 3',
+        body: 'body 3'
+      },
+    ]
   }
 
   render() {
-    const { name, counter } = this.state;
-
+    const { posts } = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p onClick={this.handlePClick}>
-            {name}
-            <br/>
-            {counter}
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-            onClick={this.handleAclick}
-          >
-            Learn React
-          </a>
-        </header>
+        {posts.map(element => (
+          <div key={element.id}>
+            <h1>
+              {element.title}
+            </h1>
+            <h5>
+              {element.body}
+            </h5>
+          </div>
+          ))}
       </div>
     )
   }
