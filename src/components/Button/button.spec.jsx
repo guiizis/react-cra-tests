72c28ac4712cf59835844747a7a228680e49ad60
guiziis <guigamoda@hotmail.com>
2023-06-19 21:59:32 -0300

    test: adiciona testes ao componente de botao

---
+++ src/components/Button/button.spec.jsx
@@ -0,0 +1,39 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import { Button } from '.';
+
+describe('<Button/>', () => {
+
+  it('should render the button with text', () => {
+    render(<Button text="load more" />);
+
+    const button = screen.getByRole('button', { name: /load more/i });
+    expect(button).toBeInTheDocument();
+  });
+
+  it('should call function on button click', () => {
+    const fn = jest.fn();
+
+    render(<Button text="load more" loadMorePosts={fn} />);
+
+    const button = screen.getByRole('button', { name: /load more/i });
+    
+    fireEvent.click(button);
+    expect(fn).toHaveBeenCalled();
+  });
+
+  it('should be disabled when disabled is true', () => {
+    render(<Button text="load more" disabled={true} />);
+
+    const button = screen.getByRole('button', { name: /load more/i });
+    
+    expect(button).toBeDisabled();
+  });
+
+  it('should be enabled when disabled is false', () => {
+    render(<Button text="load more" disabled={false} />);
+
+    const button = screen.getByRole('button', { name: /load more/i });
+    
+    expect(button).toBeEnabled();
+  });
+})
