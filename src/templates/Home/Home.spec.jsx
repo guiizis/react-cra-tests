53420d81728c396e578336d517806650da753f1b
guiziis <guigamoda@hotmail.com>
2023-06-25 11:53:08 -0300

    test: adiciona testes para inputText em home.spec.jsx

--- src/templates/Home/Home.spec.jsx
+++ src/templates/Home/Home.spec.jsx
@@ -3,6 +3,7 @@ import { setupServer } from 'msw/node';
 import { screen } from '@testing-library/react';
 import { render, waitForElementToBeRemoved } from '@testing-library/react';
 import Home from '.';
+import userEvent from '@testing-library/user-event';
 
 const handlers = [
   rest.get('*jsonplaceholder.typicode.com*', async (req, res, ctx) => {
@@ -64,5 +65,35 @@ describe('<Home/>', () => {
 
     const button = screen.getByRole('button');
     expect(button).toBeInTheDocument();
+  });
+
+  it('should search for Inputs', async () => {
+    render(<Home />);
+
+    const noMorePosts = screen.getByText('Não existem Posts');
+
+    expect.assertions(11);
+
+    await waitForElementToBeRemoved(noMorePosts);
+
+    const search = screen.getByPlaceholderText(/type your search/i);
+
+    expect(screen.getByRole('heading', { name: 'title1' })).toBeInTheDocument();
+    expect(screen.getByRole('heading', { name: 'title2' })).toBeInTheDocument();
+    expect(screen.queryByRole('heading', { name: 'title4' })).not.toBeInTheDocument();
+
+    userEvent.type(search, 'title1');
+    expect(screen.getByRole('heading', { name: 'title1' })).toBeInTheDocument();
+    expect(screen.queryByRole('heading', { name: 'title2' })).not.toBeInTheDocument();
+    expect(screen.queryByRole('heading', { name: 'title4' })).not.toBeInTheDocument();
+    expect(screen.getByRole('heading', { name: /Você pesquisou por/i })).toBeInTheDocument();
+
+    userEvent.clear(search);
+    expect(screen.getByRole('heading', { name: 'title1' })).toBeInTheDocument();
+    expect(screen.getByRole('heading', { name: 'title2' })).toBeInTheDocument();
+    expect(screen.queryByRole('heading', { name: 'title4' })).not.toBeInTheDocument();
+
+    userEvent.type(search, 'Kamino');
+    expect(screen.getByText('Não existem Posts')).toBeInTheDocument();
   });
 });
